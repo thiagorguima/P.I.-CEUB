61262a986e4bc4f8367435151b437d76004f1e44
thiagorguima <162577820+thiagorguima@users.noreply.github.com>
2025-10-02 15:56:59 -0300

    Add files via upload

---
+++ tela 4.js
@@ -0,0 +1,7 @@
+function voltar() {
+  window.history.back();
+}
+
+document.querySelector('.menu-btn').addEventListener('click', () => {
+  alert("Abrir menu de opções");
+});
---
+++ tela 5.js
@@ -0,0 +1,7 @@
+function voltar() {
+  window.history.back();
+}
+
+document.querySelector('.search-btn').addEventListener('click', () => {
+  alert("Função de busca ainda não implementada.");
+});
